61fd67e8449bd5542445c37dfa7e2ad21eb242af
abdallah <abdallahtolba17@gmail.com>
2025-07-30 23:39:45 +0300

    add api methods

---
+++ lib/core/api/api_constances.dart
@@ -0,0 +1,19 @@
+class ApiConstances {
+  static const String baseUrl = 'https://ecommerce-api.edgesgate.com';
+  static const String homeSliderPath = '/homeSliders';
+  static const String categoriesPath = '/categories';
+  static const String productsTopRatedPath = '/products/topRated';
+  static const String productsPath = '/products';
+  static const String loginPath = '/auth/login';
+  static const String logoutPath = '/auth/logout';
+  static const String accountInfoPath = '/auth/me';
+  static const String updateAccountInfoPath = '/auth/updateProfile';
+  static const String myAddressesPath = '/myAddresses';
+  static const String myOrdersPath = '/orderHistory';
+  static const String addToCart = '/cart/add';
+  static const String cart = '/cart';
+  static const String removeFromCart = '/removeFromCart';
+  static const String makeOrder = '/makeOrder';
+  static const String addAddress = '/addAddress';
+  static const String updateAddress = '/updateAddress';
+}
