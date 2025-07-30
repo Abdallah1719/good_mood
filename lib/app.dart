21602a201a2a567735309104a0cfd9db2f4367e6
abdallah <abdallahtolba17@gmail.com>
2025-07-30 23:27:20 +0300

    add theme & create cubit theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:good_mood/core/theme/cubit/theme_cubit.dart';
 import 'package:good_mood/features/splash/presentation/screens/splash_screen.dart';
 import 'package:good_mood/generated/l10n.dart';
 import 'package:good_mood/l10n/cubit/local_cubit.dart';
@@ -12,18 +13,21 @@ class App extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => LocaleCubit()..loadSavedLocale()),
+        BlocProvider(create: (context) => ThemeCubit()..loadSavedTheme()),
       ],
       child: BlocBuilder<LocaleCubit, String>(
         builder: (context, lacale) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            localizationsDelegates: LocaleCubit.localizationsDelegates,
-            locale: Locale('en'),
-            supportedLocales: S.delegate.supportedLocales,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            ),
-            home: SplashScreen(),
+          return BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return MaterialApp(
+                title: 'Good Mood',
+                localizationsDelegates: LocaleCubit.localizationsDelegates,
+                locale: Locale('en'),
+                supportedLocales: S.delegate.supportedLocales,
+                theme: context.watch<ThemeCubit>().currentTheme(),
+                home: SplashScreen(),
+              );
+            },
           );
         },
       ),
